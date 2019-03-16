0ad28c720fc4043eeb70daa0d8e12376bdfc626d
Bharath Ramaprasad <bharath.hr86@yahoo.com>
2019-03-15 21:47:05 -0700

    Update Configuration.js

--- Mid-Tier-Routing/sdb-mid-tier-routing-demo-app-backend/backends/backend-1/Configuration.js
+++ Mid-Tier-Routing/sdb-mid-tier-routing-demo-app-backend/backends/backend-1/Configuration.js
@@ -11,11 +11,6 @@
 var fs  = require('fs');
 var https = require('https');
 
-var options = {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem')
-  };
-
 var proxyOptions = { 
     xfwd: false,
     secure: false, 
@@ -34,13 +29,12 @@ var getInvoiceDetails = {
 var shardConnectionParams = {
     user          : "app_schema",
     password      : "app_schema",
-    connectString : "slc15zym:6216/sitemtrc.regress.rdbms.dev.us.oracle.com"
+    connectString : "host:port/service"
   };
 
 var registryPort = process.env.PORT || 10000; // port which the registry server will listen on.
 
 exports.getInvoiceDetails = getInvoiceDetails;
-exports.options = options;
 exports.proxyOptions = proxyOptions;
 exports.registryPort = registryPort;
 exports.shardConnectionParams = shardConnectionParams;  
